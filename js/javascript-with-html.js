6760afc423071a72ea1ea37e33e8677a884ef2eb
Valeria Canizales <vcanizales93@gmail.com>
2022-08-12 11:12:40 -0500

    first functions2

---
+++ js/javascript-with-html.js
@@ -0,0 +1,19 @@
+// alert("yo!");
+//only has the option to select OK
+
+// confirm("Are you going to eat that?");
+//^^includes a CANCEL and OK button
+
+//Function that returns a boolean
+// let userChoice = confirm("Are you going to eat that?");
+//     console.log(userChoice)
+//
+// if (userChoice === true) {
+//     alert("Mannnn");
+// } else {
+//     alert("Yay!");
+// }
+
+//no boolean here\\
+let name = prompt("What's your name?");
+    console.log(name);
