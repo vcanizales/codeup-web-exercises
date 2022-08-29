16df26898c0a76f86199108f8275be93da8ab7b8
Valeria Canizales <vcanizales93@gmail.com>
2022-08-29 16:53:33 -0500

    CSS and more javascript functions

--- warmups.js
+++ warmups.js
@@ -49,3 +49,153 @@ function getLongestString(strArray) {
 };
 
 console.log(getLongestString(array));
+
+//Write a function, calculateTotalStudents, that returns the total number of students recorded in the classes array.
+let classes = [
+    {class: "6th grade history", students: 18},
+    {class: "7th grade history", students: 20},
+    {class: "8th grade history", students: 22}
+];
+
+// function calculateTotalStudents() {
+//     let sum = classes.reduce((accumulator, object, students) => {
+//         return accumulator + object.students;
+//     }, 0);
+//     console.log(sum);
+// }
+
+function calculateTotalStudents(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i].students;
+        }
+    return sum;
+    }
+
+
+let numericArray = [8, 10, 20];
+function calculateTotal(array) {
+    let total=0;
+    for (let i=0; i < array.length; i++) {
+        total += array[i];
+    }
+    return total;
+}
+
+console.log(calculateTotal(numericArray));
+
+// Write a function called convertToObject that takes in a string that is the name of a class, and a number that is the number of students, and returns an object with the properties `class` and `students`
+
+//convertToObject("Intro to Programming", 20) returns {class: "Intro to Programming", students: 20}
+
+// function convertToObject(string, number) {
+//     let temp = convertToObject.split("",number),
+//         obj = {};
+//     for (let i=0; i<temp.length; i +=2) {
+//         obj[temp[i]] = temp[(i+1)];
+//     }
+//     console.log(obj);
+// }
+
+// const convertToObject = function(nameOfClass, numberOfStudents) {
+//     return {
+//         class: nameOfClass,
+//         numberOfStudents: numberOfStudents
+//     }
+// }
+
+const convertToObject = (nameOfClass, numberOfStudents) => {
+    return {
+        class: nameOfClass,
+        numberOfStudents: numberOfStudents
+    }
+}
+
+//////////////////////////
+console.log(convertToObject("Intro to Programming", 20));;
+
+let myFunction = convertToObject;
+console.log(myFunction("Intro to React", 6));
+
+//////////////////////////
+
+const neighborhood1 ={
+    neighborhood: "Lovely Estates",
+    medianHomePrice: 280000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 8},
+        {name: "MS2", rating: 6},
+        {name: "HS3", rating: 8}
+    ]
+}
+
+const neighborhood2 ={
+    neighborhood: "Luminous Estates",
+    medianHomePrice: 270000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 8},
+        {name: "MS2", rating: 8},
+        {name: "HS3", rating: 8}
+    ]
+}
+
+const neighborhood3 ={
+    neighborhood: "Ginormous Ego Estates",
+    medianHomePrice: 350000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 9},
+        {name: "MS2", rating: 9},
+        {name: "HS3", rating: 9}
+    ]
+}
+
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+
+function desirableNeighborhood(neighborhoodsObject) {
+    let total = 0;
+    for (let i = 0; i < neighborhoodsObject.schools.length; i++) {
+        total += neighborhoodsObject.schools[i].rating;
+    }
+        return neighborhoodsObject.medianHomePrice < 300000 && neighborhoodsObject.crimeRate === "low" && total >= 24;
+}
+
+//Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+//
+// A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+//
+// Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+//
+//     your referral bonus, and
+//
+//     the price of a beer can
+//
+// For example:
+//
+// beeramid(1500, 2); // should === 12
+// beeramid(5000, 3); // should === 16
+
+//what's the cost of each level? level*level*priceOfCan
+//need a total cost accumulator variable and some kind of level variable that keeps track of the level we are on
+//need to keep looping until total cost > bonus
+
+function beeramid(bonus, price) {
+    let totalCost = 0;
+    let levels = 0;
+    while (totalCost < bonus) {
+        totalCost += levels * levels * price;
+        if(totalCost + ((levels + 1) ** 2 * price) > bonus) {
+            break;
+        }
+        levels++;
+    }
+    return levels;
+}
