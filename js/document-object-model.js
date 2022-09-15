e4d8b65061b79a2bfe7d23e7fdd85db7f723590f
Valeria Canizales <vcanizales93@gmail.com>
2022-09-15 14:48:48 -0500

    DOM lecture and DOM exercise

--- js/document-object-model.js
+++ js/document-object-model.js
@@ -31,3 +31,37 @@ submitButton.onclick = function () {
     }
 }
 
+//placing a <div> with the DOM model in js//
+const body = document.body
+const div = document.createElement("div")
+div.innerText = "Hi Valeria"
+body.appendChild(div)
+
+//the DATA attribute//
+let theHulk = document.getElementById("hulk");
+// let hulkState = theHulk.getAttribute("data-state");
+// theHulk.innerText += " is in " + hulkState + " state";
+//following are data attributes of the hulk
+let hulkState = theHulk.dataset.state;
+let hulkStrength = theHulk.dataset.strength;
+let hulkIntelligence = theHulk.dataset.intelligence;
+let hulkSummary = `The Hulk is in ${hulkState} state. His strength is ${hulkStrength} and his intelligence is ${hulkIntelligence}.`
+// theHulk.innerText += ` is in ${hulkState} state`;
+theHulk.innerText = hulkSummary ;
+
+//reference to transformation button
+let transformButton = document.getElementById("transformHulk");
+//get a click event
+transformButton.onclick = function() {
+    let theHulk = document.getElementById("hulk");
+    let hulkState = theHulk.dataset.state;
+    let hulkStrength = theHulk.dataset.strength;
+    let hulkIntelligence = theHulk.dataset.intelligence;
+    //re-assign his state
+    if (hulkState === "hulk") {
+        theHulk.dataset.state = "Bruce Banner";
+    } else {
+        theHulk.dataset.state = "hulk";
+    }
+    theHulk.innerText = `The hulk is in ${hulkState} state.`
+}
