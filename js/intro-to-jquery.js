99f7f943c9dc829178e56751cb2442a68b472a3a
Valeria Canizales <vcanizales93@gmail.com>
2022-09-21 14:19:32 -0500

    jQuery mouse events change font size to 18px to paragraphs

--- js/intro-to-jquery.js
+++ js/intro-to-jquery.js
@@ -2,6 +2,9 @@ $('h1').click(function(){
     $(this).css('background-color', '#1969B2')
 });
 
+$('p').dblclick(function(){
+    $(this).css('font-size', '18px')
+});
 
     // Add jQuery code that will change the background color of an h1 element when clicked.
 
