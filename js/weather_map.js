3ea7f411a4f69e12d8b282023df972ffb1110930
Valeria Canizales <vcanizales93@gmail.com>
2022-09-28 16:56:12 -0500

    Weather map missing user markers

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,16 +3,54 @@ $(function() {
     mapboxgl.accessToken = MAPBOX_API_TOKEN;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        style:'mapbox://styles/mapbox/streets-v11', // style URL
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
         // 'mapbox://styles/mapbox/satellite-v9',
         center: [-80.1891, 25.8059], // starting position [lng, lat]
-        zoom: 2, // starting zoom
+        zoom: 5, // starting zoom
         projection: 'globe' // display the map as a 3D globe
+    });
+        map.on('style.load', () => {
+            map.setFog({}); // Set the default atmosphere style
     });
 
 
     //Declaring days of the week
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID,
+        lat: 29.423017,
+        lon: -98.48527,
+        units: "imperial" // this is fahrenheit
+    }).done(function (data) {
+        displayWeather(data);
+    });
+
+    //Functionality to the Search Bar
+    document.getElementById('clickMe').addEventListener('click', function(e){
+        e.preventDefault();
+        const address=document.getElementById('searchBar').value;
+        console.log(address);
+        geocode(address, MAPBOX_API_TOKEN).then(function (coordinates){
+            console.log(coordinates);
+            //Setting Markers for Search
+            const userMarker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+            map.setCenter(coordinates);
+            refreshWeather(coordinates);
+            //Empties previous search
+            $(".weather-predictions").empty();
+        });
+    });
+
+    // $.get("https://api.openweathermap.org/data/2.5/weather", {
+    //     APPID: OPEN_WEATHER_APPID,
+    //     lat: 29.423017,
+    //     lon: -98.48527,
+    //     units: "imperial"
+    // }).done(function(data) {
+    //     console.log(data);
+    //     refreshWeather(data);
+    // });
     function namedDayFromDay(timeStamp){
         let dateTime = new Date(timeStamp * 1000);
         return daysOfWeek[dateTime.getDay()];
@@ -56,38 +94,42 @@ $(function() {
         return cardinalDirection;
     }
 
-    $.get("https://api.openweathermap.org/data/2.5/forecast/", {
-        APPID: OPEN_WEATHER_APPID,
-        lat: 29.423017,
-        lon: -98.48527,
-        units: "imperial"
-    }).done(function (data) {
-        //Function to loop through days of the week with weather data
+
+    // Setting up cards with functionality to current temp
+    // $.get("https://api.openweathermap.org/data/2.5/forecast/", {
+    //     APPID: OPEN_WEATHER_APPID,
+    //     lat: 29.423017,
+    //     lon: -98.48527,
+    //     units: "imperial"
+    // }).done (
+    function displayWeather(data) {
         data.list.forEach((forecast, i) =>{
             console.log(i);
             // console.log(data.list[i]);
-            $(".weather-predictions")
-                // data.list.forEach((forecast, i) => {
-                //     console.log(data);
-                    if(i % 8 == 0) {
-                        console.log("inside if i%8===0: " + i);
-                        $(`#weather-predictions`).append(`
-                        <div class="card col-2">
-                        <p>Current Date ${data.list[i].dt_txt.split(' ')[0]}</p>
-                        <p>The current temperature is ${data.list[i].main.temp}</p>
-                        <p>Description: ${data.list[i].weather[0].description}</p>
-                        <p>Humidity: ${data.list[i].main.humidity}</p>
-                        <p>Wind Speed: ${data.list[i].wind.speed}</p>
-                        <p>Pressure: ${data.list[i].main.pressure}</p>
+            //$(".weather-predictions")
+            // data.list.forEach((forecast, i) => {
+            //     console.log(data);
+            if(i % 8 === 0) {
+                // console.log("inside if i%8===0: " + i);
+                $(`#weather-predictions`).append(`
+                        <div class="card col-2 text-center">
+                            <p>
+                                <img class="card-img-top" src="http://openweathermap.org/img/w/${data.list[8].weather[0].icon}.png"  alt="Card image cap" style="height: 100px; width: 100px;">
+                            </p>
+                            <p>Current Date ${data.list[i].dt_txt.split(' ')[0]}</p>
+                            <p>The current temperature is ${data.list[i].main.temp}</p>
+                            <p>Description: ${data.list[i].weather[0].description}</p>
+                            <p>Humidity: ${data.list[i].main.humidity}</p>
+                            <p>Wind Speed: ${data.list[i].wind.speed}</p>
+                            <p>Pressure: ${data.list[i].main.pressure}</p>
                         </div>`);
-                    }
-                // });
+            }
         });
-    });
+    };
+    // });
 
-
-    function updateWeather(coordinates) {
-        console.log("inside updateWeather");
+    //Function to update the weather
+    function refreshWeather(coordinates) {
         console.log(coordinates[0]);
         console.log(coordinates[1]);
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
@@ -97,22 +139,110 @@ $(function() {
             units: "imperial" // this is fahrenheit
         }).done(function (data) {
             console.log("completed updateWeather get request");
-            showWeather(data);
+            displayWeather(data);
         });
     }
-    $.get("https://api.openweathermap.org/data/2.5/weather", {
-        APPID: OPEN_WEATHER_APPID,
-        lat: 29.423017,
-        lon: -98.48527,
-        units: "imperial"
-    }).done(function(data) {
-        console.log(data);
-    });
-
-
-
 
 });
+
+    //Function to set markers by user with a pulsating dot
+    const size = 200;
+    const pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+
+    // When the layer is added to the map,
+    // get the rendering context for the map canvas.
+        onAdd: function () {
+            const canvas = document.createElement('canvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext('2d');
+        },
+
+    // Call once before every frame where the icon will be used.
+        render: function () {
+            const duration = 1000;
+            const t = (performance.now() % duration) / duration;
+
+            const radius = (size / 2) * 0.3;
+            const outerRadius = (size / 2) * 0.7 * t + radius;
+            const context = this.context;
+
+    // Draw the outer circle.
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                outerRadius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
+            context.fill();
+
+    // Draw the inner circle.
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                radius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(255, 100, 100, 1)';
+            context.strokeStyle = 'white';
+            context.lineWidth = 2 + 4 * (1 - t);
+            context.fill();
+            context.stroke();
+
+    // Update this image's data with data from the canvas.
+            this.data = context.getImageData(
+                0,
+                0,
+                this.width,
+                this.height
+            ).data;
+
+    // Continuously repaint the map, resulting
+    // in the smooth animation of the dot.
+            map.triggerRepaint();
+
+    // Return `true` to let the map know that the image was updated.
+            return true;
+        }
+    };
+
+    map.on('click',function (e){
+        map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+        map.addSource('dot-point', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': [
+                    {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [0, 0] // icon position [lng, lat]
+                        }
+                    }
+                ]
+            }
+        });
+        map.addLayer({
+            'id': 'layer-with-pulsing-dot',
+            'type': 'symbol',
+            'source': 'dot-point',
+            'layout': {
+                'icon-image': 'pulsing-dot'
+            }
+        });
+});
+
 
 
 
